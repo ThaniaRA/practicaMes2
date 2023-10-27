548b517bfda40da715cf93f828204bcf22c084ba
Thania <20200787@uthh.edu.mx>
2023-10-27 05:20:30 -0600

    se esta subiendo

---
+++ resources/views/Blog.blade.php
@@ -0,0 +1,264 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog Alfredito Olivares</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Free HTML Templates" name="keywords" />
+    <meta content="Free HTML Templates" name="description" />
+      
+    
+    <!-- Flaticon Font -->
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet" />
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon" />
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Handlee&family=Nunito&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Font Awesome -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+      rel="stylesheet"
+    />
+
+    <!-- Flaticon Font -->
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet" />
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-light position-relative shadow">
+      <nav
+        class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 px-lg-5"
+      >
+        <a
+          href=""
+          class="navbar-brand font-weight-bold text-secondary"
+          style="font-size: 50px"
+        >
+        <Center>
+          <i class="flaticon-043-teddy-bear"></i>
+          <span class="text-primary">BLOG DE ALFREDO OLIVAS "EL PATRONCITO"</span>
+        </a>
+
+</Center>
+        <button
+          type="button"
+          class="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-between"
+          id="navbarCollapse"
+        >
+          
+          </div>
+        </div>
+      </nav>
+    </div>
+    <!-- Navbar End -->
+
+    <!-- Header Start -->
+    <div class="container-fluid bg-primary px-0 px-md-5 mb-5">
+      <div class="row align-items-center px-3">
+        <div class="col-lg-6 text-center text-lg-left">
+        
+          <h1 class="display-3 font-weight-bold text-white">
+          José Alfredo Olivas Rojas
+          </h1>
+          <div></div>
+          <H5 class="text-white mb-4 mt-5 mt-lg-0">
+            "EL PASADO DEJO CICATRICES, PERO CREO QUE EL TIEMPO LAS ELIMINO"
+            <div> -Alfredo Olivas</div>
+          </H5>
+          
+        </div>
+        <div class="col-lg-6 text-center text-lg-right">
+        <img class="img-fluid mt-5" src="/img/afl.jpg" width="700" height="200" />
+        </div>
+      </div>
+      
+    </div>
+    <!-- Header End -->
+
+<!-- About Start -->
+<div class="container-fluid py-5">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-5">
+        <img
+          class="img-fluid rounded mb-5 mb-lg-0"
+          src="img/alfre.jpg"
+          alt=""
+        />
+      </div>
+      <div class="col-lg-7">
+        <p class="section-title pr-5">
+          <H2> <span class="pr-2">Conoce a </span>
+        </p>
+        <h1 class="mb-4">José Alfredo Olivas Rojas</h1>
+        <p>
+        José Alfredo Olivas Rojas nació un 1° de octubre en Ciudad Obregón, Sonora.
+          <p>
+          A los 9 años de edad compuso su primer corrido anticipando su formación como cantautor, categoría generalmente reservada para artistas de mayor edad.
+          </p>
+          
+         <p>
+         Realizó su primera grabación a los 12 años, el proyecto Alfredito, el sierreño. Sin embargo, inauguró su carrera de manera profesional cinco años después con el lanzamiento del álbum El patroncito (2011), y con el tema Con la novedad.
+          <p>
+          Otras obras de su autoría que lo han consolidado como autor son La mejor de las historias, El día de los muertos, En definitiva, El problema, Antecedentes de culpa, La rueda de la fortuna, El laberinto, Ya no es normal, Sin memoria (dueto con Julión Álvarez), El sillón, El zoológico (dueto con El Fantasma), Volverá, Tres tiros (dueto con Los Chavalos de la Perla) y El paciente, distinguida con el galardón Éxito sacm 2019 como una de las canciones más sonadas del año anterior, otorgado por la Sociedad de Autores y Compositores de México.s
+          </p>
+         </p>
+     
+        </p>
+        <div class="row pt-2 pb-4">
+          <div class="col-6 col-md-4">
+            
+          </div>
+          <div class="col-6 col-md-8">
+            <ul class="list-inline m-0">
+              
+               
+            </ul>
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  </div>
+</div>
+<!-- About End -->
+   
+
+
+    <!-- Class Start -->
+    <div class="container-fluid pt-5">
+      <div class="container">
+        <div class="text-center pb-2">
+          <p class="section-title px-5">
+           
+          </p>
+          <h1 class="mb-4">Estos son las tres canciones mas escuchadas de Alfredo Olivas</h1>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 mb-5">
+            <div class="card border-0 bg-light shadow-sm pb-2">
+              <img class="card-img-top mb-2" src="img/dd.png" alt="" />
+              <div class="card-body text-center">
+                <h4 class="card-title">EL PACIENTE</h4>
+                <p class="card-text">
+                En primer lugar se encuentra el tema El paciente. Se compuso luego de una mala experiencia cerca de la muerte cuando fue baleado por algunos asistentes en una presentación en Parral, Chihuahua en el año 2015.
+                </p>
+              </div>
+             
+             
+            </div>
+          </div>
+          <div class="col-lg-4 mb-5">
+            <div class="card border-0 bg-light shadow-sm pb-2">
+              <img class="card-img-top mb-2" src="img/mo.png" alt="" />
+              <div class="card-body text-center">
+                <h4 class="card-title">EN DEFINITIVA</h4>
+                <p class="card-text">
+                En definitiva se posiciona en el segundo lugar; es de desamor y narra cómo ahora sí la relación terminó para siempre; tiene 139 millones 583 mil 956 vistas en Youtube y en Spotify 127 millones 692 mil 918 reproducciones.
+                </p>
+             
+                <div class="row">
+                  <div class="col-6 py-1 text-right border-right">
+                    
+                  </div>
+                  
+                </div>
+              </div>
+            
+            </div>
+          </div>
+          <div class="col-lg-4 mb-5">
+            <div class="card border-0 bg-light shadow-sm pb-2">
+              <img class="card-img-top mb-2" src="img/vid.png" alt="" />
+              <div class="card-body text-center">
+                <h4 class="card-title">EL PRECIO DE LA SOLEDAD</h4>
+                <p class="card-text">
+                El precio de la soledad es el tema que está en tercera posición, publicado en 2012. Tiene 18 millones 010 mil 918 vistas en Youtube y en Spotify 35 millones 790 mil 644 reproducciones.
+                </p>
+              </div>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Class End -->
+
+  
+   
+
+    <!-- Footer Start -->
+    <div
+      class="container-fluid bg-secondary text-white mt-5 py-5 px-sm-3 px-md-5"
+    >
+      <div class="row pt-5">
+        <div class="col-lg-3 col-md-6 mb-5">
+          <a
+            href=""
+            class="navbar-brand font-weight-bold text-primary m-0 mb-4 p-0"
+            style="font-size: 40px; line-height: 40px"
+          >
+           
+          <form action="">
+            <div class="form-group">
+              
+            </div>
+            <div class="form-group">
+              
+            </div>
+            <div>
+              
+            </div>
+          </form>
+        </div>
+      </div>
+      <div
+        class="container-fluid pt-5"
+        style="border-top: 1px solid rgba(23, 162, 184, 0.2) ;"
+      >
+       <center>
+  <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+  Blog de Alfredo Olivas
+  
+  
+  <br />Actividad Realizada por:: THANIA RIVERA ADAN
+  
+</p>
+</div>
+</div>
+<!-- Footer End -->
+
+  </body>
+</html>
